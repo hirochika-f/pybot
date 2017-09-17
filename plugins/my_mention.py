1f7ec1041bb7556762b327a34d705955995cf5da
mac_hirochika <pawapuro.penspinner@gmail.com>
2017-09-17 19:59:27 +0900

    modified my_mention.py import settgin

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -1,7 +1,7 @@
 # _*_coding: utf-8_*_
 
 from slackbot.bot import respond_to     # @botname: で反応するデコーダ
-from plugins.scripts.confirm_weather import ConfirmWeather
+from plugins.scripts.hour_rain import HourRain
 
 @respond_to('(.雨*)')
 def confirm_weather(message, something):
