89742d9997f012bcd8fee4a238a01389063b6526
mac_hirochika <pawapuro.penspinner@gmail.com>
2017-09-18 00:22:16 +0900

    modified my_mention.py bug

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -3,12 +3,12 @@
 from slackbot.bot import respond_to     # @botname: で反応するデコーダ
 from plugins.scripts.hour_rain import HourRain
 
-#@respond_to('(雨海老名)')
-#def confirm_weather_e(message, something):
-#  weather_class = HourRain("e")
-#  weather_class.return_rainfall(message)
+@respond_to('(雨海老名)')
+def confirm_weather_e(message, something):
+  weather_class = HourRain(mode="e")
+  weather_class.return_rainfall(message)
 
-@respond_to('雨')
+@respond_to('(雨)')
 def confirm_weather(message, something):
   weather_class = HourRain()
   weather_class.return_rainfall(message)
