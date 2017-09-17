4561a65c029f1926b9799db178778a00447b603c
mac_hirochika <pawapuro.penspinner@gmail.com>
2017-09-18 00:26:56 +0900

    modified my_mention.py bug

--- plugins/scripts/hour_rain.py
+++ plugins/scripts/hour_rain.py
@@ -2,17 +2,17 @@ import urllib.request
 import json
 import os
 
-class HourRain(mode=None):
+class HourRain():
 
   def __init__(self):
 
     # YahooAPI の URL
     base_url = "https://map.yahooapis.jp/weather/V1/place?"
     # 調べたい場所の経度緯度
-    if mode is None:
-      coordinates = "139.620759,35.511117"
-    else:
-      coordinates = "139.388959,35.459854"
+    #if mode is None:
+    coordinates = "139.620759,35.511117"
+    #else:
+    #  coordinates = "139.388959,35.459854"
     # ClientID
     client_id = os.environ["YAHOO_API_ID"]
     self.url = "%scoordinates=%s&appid=%s&output=json" % (base_url, coordinates, client_id)
@@ -28,10 +28,10 @@ class HourRain(mode=None):
     for index, var in enumerate(weather_info):
         info = self.return_rain_level(var["Rainfall"])
         if index == 0:
-          if mode is None:
-            before_words = "新横浜では今、"
-          else:
-            before_words = "海老名では今、"
+          #if mode is None:
+          before_words = "新横浜では今、"
+          #else:
+           # before_words = "海老名では今、"
           if var['Rainfall'] == 0.0:
             after_words = "っていない"
           else:
