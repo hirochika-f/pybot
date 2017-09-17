7231482a7c9fbef01b9c2f6003cf0c523ee4caf1
mac_hirochika <pawapuro.penspinner@gmail.com>
2017-09-17 23:42:16 +0900

    modified my_mention.py regex

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -3,7 +3,7 @@
 from slackbot.bot import respond_to     # @botname: で反応するデコーダ
 from plugins.scripts.hour_rain import HourRain
 
-@respond_to(r'.雨*')
+@respond_to('.雨*')
 def confirm_weather(message, something):
   weather_class = HourRain()
   weather_class.return_rainfall(message)
