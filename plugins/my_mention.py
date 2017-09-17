20fc249416605c20eec1bd88851f31c447a714cd
mac_hirochika <pawapuro.penspinner@gmail.com>
2017-09-18 00:14:50 +0900

    modified my_mention.py accept ebina

--- plugins/my_mention.py
+++ plugins/my_mention.py
@@ -4,7 +4,7 @@ from slackbot.bot import respond_to     # @botname: で反応するデコーダ
 from plugins.scripts.hour_rain import HourRain
 
 @respond_to('(雨海老名)')
-def confirm_weather(message, something):
+def confirm_weather_e(message, something):
   weather_class = HourRain("e")
   weather_class.return_rainfall(message)
 
